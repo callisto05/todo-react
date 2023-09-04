b630d9e757843694eb491eed09e4b21fa1843780
prathamesh bhosale <prathamesh341@gmail.com>
2023-09-04 10:03:27 +0530

    Initial commit for todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import TodoTable from "./components/TodoTable";
+import NewTodoForm from "./components/NewTodoForm";
 
 function App() {
+  const [todos, setTodos] = useState([
+    { rowNumber: 1, rowDescription: "Feed puppy", rowAssigned: "Eric" },
+    { rowNumber: 2, rowDescription: "Get haircut", rowAssigned: "Eric" },
+    { rowNumber: 3, rowDescription: "Water the plants", rowAssigned: "Eric" },
+    {
+      rowNumber: 4,
+      rowDescription: "Charge phone battery",
+      rowAssigned: "Eric",
+    },
+  ]);
+
+  const addTodo = (description, assigned) => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: description,
+        rowAssigned: assigned,
+      };
+      setTodos((todos) => [...todos, newTodo]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Your Todo's</div>
+        <div className="card-body">
+          <TodoTable todos={todos} />
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add new todo
+          </button>
+          <NewTodoForm addTodo={addTodo} />
+        </div>
+      </div>
     </div>
   );
 }
